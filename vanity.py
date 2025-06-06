a59846aa58797635a09981da07d6618e92aeffb6
whoisibk <oyetimehinjoseph@gmail.com>
2025-06-06 11:40:18 +0100

    Adding Comments

--- vanity.py
+++ vanity.py
@@ -7,35 +7,38 @@ def main():
 
 
 def is_valid(s):
-    if cond_1(s) and cond_2(s) and cond_3(s) and cond_4(s) and cond_5(s):
+    #making sure ALL condition are satisified
+    if cond_1(s) and cond_2(s) and cond_3(s) and cond_4(s) and cond_5(s): 
         return True
     else:
         return False
 
-def cond_1(s):
+def cond_1(s): #first condition
     while len(s) > 1:
+        #making sure 1st 2nd letter are always letters
         if s[0].isalpha() and s[1].isalpha():
               return True
         else:
             return False
 
-def cond_2(s):
-    if  1 < len(s) < 7:
+def cond_2(s): #second condition
+    if  1 < len(s) < 7: #making sure the length is between 1 and 7
         return True
 
-def cond_3(s):
-    if s.isalpha():
+def cond_3(s): #third condition
+    if s.isalpha(): #s can be made up of letters only
         return True
-    elif s.isalnum():
-        while len(s) >= 4:
-            if (s[-1].isdigit() and s[-2].isdigit()):
+    elif s.isalnum(): #letters and numbers
+        while len(s) >= 4: #while length >= 4
+            if (s[-1].isdigit() and s[-2].isdigit()): #true if last and penultimate are numbers
                 return True
             else:
                 return False
 
 
-def cond_4(s):
-    if  s[2] == "0":
+def cond_4(s):#4th condition
+    #checking if first number is 0
+    if  s[2] == "0": 
         return False
     if not (s[2].isalpha() and s[3] == 0):
         return True
@@ -49,8 +52,9 @@ def cond_4(s):
         return False
 
 
-def cond_5(s):
-    puncs = ".?,&'`"
+def cond_5(s): #5th condition
+    #checking if any punctuation in s
+    puncs = ".?,&'`" 
     if not any(punc in s for punc in puncs):
         return True
 
